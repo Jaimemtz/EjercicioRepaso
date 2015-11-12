bae7bfbe809407fef35005ab6c35d04fd8472491
Jaime Martínez <jaime.martinez.ortega@gmail.com>
2015-11-12 18:31:15 +0100

    Terminado el ejercicio de Repaso4

---
+++ src/Repaso4.java
@@ -0,0 +1,43 @@
+/*
+ *  Jaime Martínez:
+ * 
+ * La clase Repaso2 tiene que dibujar un rectángulo
+ * con la esquina superior izquierda en el centro de la pantalla
+ * 
+ */
+import acm.graphics.*;
+
+public class Repaso4 extends acm.program.GraphicsProgram{
+
+	//Declaro una variable de instancia para almacenar el rectángulo
+	// como es de instancia, se ve desde el init y desde el run
+	GRect rectangulo;
+
+	//declaro otra variable de instancia que guardará el punto X de la 
+	//pantalla en que empezará a pintarse el rectángulo
+	double distanciaX;
+	//declaro otra variable de instancia que guardará el punto Y de la 
+	//pantalla en que empezará a pintarse el rectángulo
+	double distanciaY;
+
+	public void init(){
+		//cambio el tamaño de la pantalla
+		setSize(800, 600);
+
+		//creo el rectángulo de 120 de ancho por 80 de alto
+		rectangulo = new GRect(120,80);
+		add(rectangulo);
+	}
+
+	public void run(){
+		//divido el ancho de la pantalla en 2 para saber donde está
+		//la mitad exacta de la pantalla
+		distanciaX = getWidth()/2 - rectangulo.getWidth()/2;
+		
+		//divido el alto de la pantalla en 2 para saber donde está
+		//la mitad exacta de la pantalla
+		distanciaY = getHeight()/2 - rectangulo.getHeight()/2;
+
+		rectangulo.setLocation(distanciaX, distanciaY);
+	}
+}
